47f72fc17dded16ed551542dc9e322f5af4f9d41
MinhaShukoor <minnup2002@gmail.com>
2024-02-26 08:45:14 +0530

    collection-navbars

--- frontend/src/Components/User/Collections/Collections.js
+++ frontend/src/Components/User/Collections/Collections.js
@@ -1,31 +1,35 @@
 import React from 'react'
-
+import { HiOutlineShoppingBag } from "react-icons/hi2";
 import './collections.css'
 
 function Collections() {
   return (
-    <div className='header-Collection'>
-
+<div className='body'>
+    <div className='headerCollect'>
     <nav>
-            <ul className='navlist'>
-            <li><a href="/" >HOME</a></li>
-    
-            </ul>
-            </nav>
-
-
- <img className="Collectionimg" src="\collectionss.png"alt="Collectionimg"/>
- <h1 className='collectionheading'>CLASSIC GLORY</h1>
- <nav>
-        <ul className='collectionsnav'>
-          <li><a href='/user/neklace'>NECKLACES</a></li>
+      <ul className='navbarlist'>
+            {/* <li><a href="/" >HOME</a></li> */}
+            <li><a href='/user/neklace'>NECKLACES</a></li>
           <li><a href='/user/bracelet'>BRACELETS</a></li>
           <li><a href='/user/earrings'>EARRINGS</a></li>
 
           <li><a href='/user/rings'>RINGS</a></li>
+ 
+    
+            </ul>
+            </nav>
+<div className='Collectheader'>
+  <nav>
+    <ul className='listnavbar'>
+    <li><a href='/'>HOME</a></li>
+          <a href='/cart' className='icon'><HiOutlineShoppingBag /></a>
+    </ul>
+  </nav>
+</div>
+  <img className="Collectionimg" src="\collectionss.png"alt="Collectionimg"/>
+ <h1 className='collectionheading'>CLASSIC GLORY</h1>
 
-        </ul>
-      </nav>
+     </div>
      </div>
   )
 }
